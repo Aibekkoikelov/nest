a11e8f269526247dc16be770d84f82934886c090
Aibek <aibekkoikelov@gmail.com>
2022-12-29 16:09:17 +0600

    module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,13 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import  {AppDataSource} from "./db/data.source";
-import { CatalogModule } from './catalog/catalog.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(),TypeOrmModule.forRoot(AppDataSource), UserModule, CatalogModule],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot(),TypeOrmModule.forRoot(AppDataSource), UserModule], //  В этом модуле подключаем все модули приложения
+  controllers: [AppController], //
   providers: [AppService],
 })
 export class AppModule {}
+
+// ConfigModule.forRoot() - это  встроенный модуль NestJS, который позволяет загружать переменные окружения из файла .env в процессе выполнения приложения. По умолчанию он загружает файл .env в корневой директории проекта. Если вы хотите загрузить другой файл, вы можете передать его имя в качестве параметра. Например, ConfigModule.forRoot({envFilePath: '.env.dev'}) загрузит файл .env.dev вместо .env.
+// TypeOrmModule.forRoot(AppDataSource) - это модуль TypeORM, который позволяет подключиться к базе данных. В качестве параметра он принимает объект, который содержит настройки подключения к базе данных. В нашем случае мы используем объект AppDataSource, который мы определили в файле db/data.source.ts. Это позволяет нам разделить настройки подключения к базе данных и настройки модуля TypeORM.
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,7 +3,9 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
 @Module({
-  controllers: [UserController],
-  providers: [UserService]
+  imports: [],
+  controllers: [UserController], // обязательно указываем контроллер в модуле
+  providers: [UserService], // В providers мы указываем сервисы, которые будут использоваться в контроллерах этого модуля.
+  exports: [UserService] // Экспортируем сервис, чтобы он был доступен в других модулях
 })
 export class UserModule {}
